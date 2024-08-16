c7f961e22612860c5f42666afbd6988ef367aaed
Benny <benny.soung@gmail.com>
2024-08-16 15:35:44 +0800

    0816製版：anime, cards_received, inventory. 修改main版面,css,js.

--- main.js
+++ main.js
@@ -5,34 +5,38 @@ $(document).ready(function(){
   if (location.pathname.indexOf("index") == -1){
     $("#wallet_address").html(new_wallet_add);
   }
-  /*  main.html -------------------------------------
-      if have 50 cards, please execute this code :
-    $("#needcard_txt").remove();
-    $("#buy_btn").addClass("off");
-    $("#buy_btn").removeClass("on");
-
-      if page of inventory have cards :
-    $("#inventory_btn").addClass("on");
-    $("#inventory_btn").removeClass("off");
-
-      if player can start play :
-    $("#start_btn").addClass("on");
-    $("#start_btn").removeClass("off");
-    $("#start_btn").attr("onclick", "pop_alert()");
-*/
   
 });
-function pop_alert() {
-  $("#pop_up_box").addClass("up");
-  $("#pop_up_box").removeClass("noup");
+function place_alert() {
+  $("#place_bet_box").addClass("up");
+  $("#place_bet_box").removeClass("noup");
 }
-function pop_cancel() {
-  $("#pop_up_box").addClass("noup");
-  $("#pop_up_box").removeClass("up");
+function place_cancel() {
+  $("#place_bet_box").addClass("noup");
+  $("#place_bet_box").removeClass("up");
 }
 
-function gotest() {
-  /*const webadd = "./cards_received.html";
-  $("#drawcards_mp4").remove();
-  window.location.href = webadd;*/
+function goAnime() {
+  const web_anime = "./anime.html";
+  window.location.href = web_anime;
 }
+function goReceived() {
+  const web_received = "./cards_received.html";
+  $("#drawcards_mp4").remove();
+  window.location.href = web_received;
+}
+function goInventory() {
+  const web_inventory = "./inventory.html";
+  window.location.href = web_inventory;
+}
+
+function warning_pop_alert() {
+  $("#warning_pop_box").addClass("up");
+  $("#warning_pop_box").removeClass("noup");
+}
+function warning_pop_cancel() {
+  $("#warning_pop_box").addClass("noup");
+  $("#warning_pop_box").removeClass("up");
+}
+
+
